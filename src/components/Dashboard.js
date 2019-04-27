edfdf6882d0139561d87e3f0667d9e0ce16798da
Mateusz Bielak <mateusz.bielak@skygate.io>
2019-04-27 11:28:11 +0200

    Add loading notification

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,12 +4,16 @@ export const Dashboard = () => {
     const [recipes, setRecipes] = useState([]);
     const [query, setQuery] = useState('');
     const [url, setUrl] = useState(`/api/?p=1`);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchData = async () => {
-            const a = await fetch(url).then(res => res.json());
+            setIsLoading(true);
 
-            setRecipes(a.results);
+            const { results } = await fetch(url).then(res => res.json());
+
+            setRecipes(results);
+            setIsLoading(false);
         };
 
         fetchData();
@@ -19,9 +23,16 @@ export const Dashboard = () => {
         <>
             <input type="text" value={query} onChange={event => setQuery(event.target.value)} />
             <button onClick={() => setUrl(`/api/?i=${query}&p=1`)}>Search</button>
-            {recipes.map(({ href, title }) => (
-                <p key={href}>{title}</p>
-            ))}
+            {recipes.length !== 0 ? (
+                recipes.map(({ href, title }) => <p key={href}>{title}</p>)
+            ) : isLoading ? (
+                <p>Leading...</p>
+            ) : (
+                <>
+                    <p>Sorry your query return any recipes. Please broaden your search.</p>
+                    <p>Input comma separated inputs, in example: chicken, paprika, onions</p>
+                </>
+            )}
         </>
     );
 };
